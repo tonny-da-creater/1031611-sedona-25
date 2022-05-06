947f8b857eb30b17d2275f0ec8d34ebd65a1df56
tonny-da-creater <toni.belovanov96@mail.ru>
2022-05-06 16:25:16 +0300

    исправил js код, перменные, тень в меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
-// navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
